98f6d0e2bec0804b7dc76863a79c5677d8964097
r4bb1t <r4bb1t.github@gmail.com>
2023-11-11 06:30:13 +0900

    feat: add nfc

--- app/src/main/java/org/techtown/hackytalky/view/MainActivity.java
+++ app/src/main/java/org/techtown/hackytalky/view/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.ClipData;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.nfc.tech.TagTechnology;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -22,7 +23,19 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.techtown.hackytalky.R;
-
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.tech.IsoDep;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcB;
+import android.nfc.tech.NfcF;
+import android.nfc.tech.NfcV;
 public class MainActivity extends AppCompatActivity {
     private final ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
@@ -62,9 +75,63 @@ public class MainActivity extends AppCompatActivity {
         FirebaseMessaging.getInstance().getToken().addOnSuccessListener(new OnSuccessListener<String>() {
             @Override
             public void onSuccess(String token) {
-                Log.d("FCM Log", "Refreshed token: "+token);
+                Log.d("FCM Log", "Refreshed token: " + token);
                 // 토큰 값 받아옴!!!
             }
         });
+    }
+
+    private final String[][] techList = new String[][]{
+            new String[]{
+                    NfcA.class.getName(),
+                    NfcB.class.getName(),
+                    NfcF.class.getName(),
+                    NfcV.class.getName(),
+                    NdefFormatable.class.getName(),
+                    TagTechnology.class.getName(),
+                    IsoDep.class.getName(),
+                    MifareClassic.class.getName(),
+                    MifareUltralight.class.getName(),
+                    Ndef.class.getName()
+            }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_IMMUTABLE);
+// 특정한 NFC 이벤트가 발생했을 때 인텐트를 생성합니다.
+        IntentFilter filter = new IntentFilter();
+// 태그가 스마트 폰 근처로 접근했을 때 이벤트가 발생합니다.
+        filter.addAction(NfcAdapter.ACTION_TAG_DISCOVERED);
+        filter.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filter.addAction(NfcAdapter.ACTION_TECH_DISCOVERED);
+// NFC 이벤트로부터 인텐트를 얻어 디스패치를 활성화합니다.
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, new IntentFilter[]{filter}, this.techList);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent.getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
+            Log.d("NFC Log", "ACTION_TAG_DISCOVERED");
+        }
+    }
+
+    // 얻어낸 바이트 배열을 문자열로 반환합니다.
+    private String ByteArrayToHexString(byte[] array) {
+        int i, j, in;
+        String[] hex = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+        String out = "";
+
+        for (j = 0; j < array.length; ++j) {
+            in = (int) array[j] & 0xff;
+            i = (in >> 4) & 0x0f;
+            out += hex[i];
+            i = in & 0x0f;
+            out += hex[i];
+        }
+        return out;
     }
 }
